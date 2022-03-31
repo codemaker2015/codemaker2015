ac2a4404167cbbef0f93693fb680b018964cd108
Vishnu Sivan <codemaker2015@gmail.com>
2022-03-31 20:33:43 +0530

    Update markdown generator

--- connect4/markdown_generator.rb
+++ connect4/markdown_generator.rb
@@ -24,7 +24,38 @@ class MarkdownGenerator
         # Hey, I'm Vishnu 👋
 
         <img src="codemaker.png" alt="Vishnu Sivan" />
-        
+
+        <p align="left">
+          <a href="https://github.com/codemaker2015/codemaker2015">
+            <img src="https://komarev.com/ghpvc/?username=codemaker2015&color=red" alt="profile views" />
+          </a>
+          <a href="https://github.com/codemaker2015/codemaker2015">
+            <img src="https://visitor-badge.glitch.me/badge?page_id=page.id" alt="page views" />
+          </a>
+          <a href="https://stackoverflow.com/users/7103882">
+            <img alt="Stack Exchange reputation" src="https://img.shields.io/stackexchange/stackoverflow/r/7103882?color=orange&label=reputation&logo=stackoverflow">
+          </a>
+          <a href="https://www.hackerrank.com/codemaker2015">
+            <img alt="HackerRank" src="https://img.shields.io/badge/hackerrank-15+-green?color=green&logo=hackerrank">
+          </a>
+          <a href="https://codemaker2015.medium.com">
+            <img alt="Medium" src="https://img.shields.io/badge/medium-40+-lightgrey?color=lightgrey&logo=medium">
+          </a>
+          <a href="https://github.com/codemaker2015?tab=followers">
+            <img alt="GitHub followers" src="https://img.shields.io/github/followers/codemaker2015?color=yellow&logo=github">
+          </a>
+          <a href="https://www.linkedin.com/in/codemaker2015">
+            <img alt="Linkedin followers" src="https://img.shields.io/badge/followers-1.9K-blue?color=blue&logo=linkedin">
+          </a>
+          <a href="https://twitter.com/codemaker2014">
+            <img alt="Twitter followers" src="https://img.shields.io/badge/followers-1-blue?color=orange&logo=twitter">
+          </a>
+          <a href="https://www.facebook.com/vishnu.sivan.754">
+            <img alt="Facebook friends" src="https://img.shields.io/badge/friends-1.9K-blue?color=yellowgreen&logo=facebook">
+          </a>
+        </p>
+
+
         ## 🚀 About Me
 
         #### **Vishnu Sivan**, Immersive Tech Lead, Game Developer, Mobile App Developer, Full Stack Developer, Web Developer
